69df09687e826e57824be37c7c29f98bd1f2b60e
kumpulanremaja <47444671+kumpulanremaja@users.noreply.github.com>
2020-05-12 22:53:10 +0700

    Update bad.sh

--- bad.sh
+++ bad.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-# Script BadVPn SSH
-# www.hidessh.com
+#screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100
+#screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200
+#screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 
-
-wget -O /usr/bin/badvpn-udpgw "https://www.dropbox.com/s/tgkxdwb03r7w59r/badvpn-udpgw"
-chmod +x /usr/bin/badvpn-udpgw
-sed -i '$ i\badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000 --max-connections-for-client 10' /etc/rc.local
-sed -i '$ i\badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000 --max-connections-for-client 10' /etc/rc.d/rc.local
-sed -i '$ i\badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000 --max-connections-for-client 10' /etc/rc.local
-sed -i '$ i\badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000 --max-connections-for-client 10' /etc/rc.d/rc.local
-sed -i '$ i\badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10' /etc/rc.local
-sed -i '$ i\badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10' /etc/rc.d/rc.local
-
-badvpn-udpgw --listen-addr 127.0.0.1:7100
-badvpn-udpgw --listen-addr 127.0.0.1:7200
-badvpn-udpgw --listen-addr 127.0.0.1:7300
-
-
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
