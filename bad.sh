6ecf29c772d448b5a41ee4e267f97fc20750e3e6
kumpulanremaja <47444671+kumpulanremaja@users.noreply.github.com>
2020-05-12 23:47:01 +0700

    Update bad.sh

--- bad.sh
+++ bad.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100
-#screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200
-#screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
-
+# Created by Hidessh
+# Modified by Hidessh
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
